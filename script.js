1cfb1546c3feb466c3ce9e607de77769b3f8e5f8
FK <dedsecfk1121@gmail.com>
2025-10-03 17:24:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// This file is currently empty as all functionality is handled by HTML/CSS and Tailwind.
+// You can add custom JavaScript logic here in the future.
+
+console.log('Script loaded successfully.');
